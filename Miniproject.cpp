ac16ff1daedb775042b6f1de7575a8f4492ee703
Sundar7577 <85982463+Sundar7577@users.noreply.github.com>
2021-08-22 09:03:47 +0545

    Update Miniproject.cpp

--- Miniproject.cpp
+++ Miniproject.cpp
@@ -1,25 +1,29 @@
-#include<stdio.h>
+#include<iostream>
 #include<stdlib.h>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
 struct node
 {
 	int data;
 	struct node *next;
+	
 };
-struct node *front=0;
 struct node *rear=0;
-
+struct node  *front=0;
 void enqueue()
 {
 	struct node *newnode;
-	int num;
-	printf("Enter the value you want to insert\n");
-	scanf("%d",&num);
+	string name;
+	cout<<"Enter the value you want to insert\n";
+	getline(cin,name);
 	newnode = (struct node *)malloc(sizeof(struct node));
-	newnode->data = num;
+	newnode->data=stoi(name);
 	newnode->next = 0;
 	if(front==0 && rear ==0)
 	{
-		front=rear=newnode;
+		front=rear;
+		rear=newnode;
 	}
 	else
 	{
@@ -27,98 +31,90 @@ void enqueue()
 		rear = newnode;
 	}
 }
-
 void dequeue()
 {
 	struct node *temp;
 	temp = front;
 	if(front==0 && rear==0)
 	{
-		printf("Queue is empty\n");
+		cout<<"Queue is empty\n";
 	}
 	else if(front == rear)
 	{
-		printf(" %d",front->data);
+		cout<<"Dequeued element is "<<front->data;
 		front = front->next;
 		free(temp);
 		front =rear = 0;
 	}
 	else
 	{
-		printf(" %d",front->data);
+		cout<<"Dequeued element is "<<front->data;
 		front = front->next;
 		free(temp);
 	}
 }
-
 void display()
 {
 	struct node * temp;
 	if(front==0 && rear == 0)
 	{
-		printf("Queue is empty\n");
+		cout<<"Queue is empty\n";
 	}
 	else
 	{
 		temp = front;
-		printf("Your Queue is:\n");
+		cout<<"Your Queue is:\n";
 		while(temp!=0)
 		{
-			printf("%d\t",temp->data);
+			cout<<"\t",temp->data;
 			temp = temp->next;
 		}
-	}
-}
-
-void isEmpty()
+	}}
+	void isEmpty()
 {
 	if(front==0 && rear ==0)
 	{
-		printf("Queue is empty\n");
+		cout<<"Queue is empty\n";
 	}
 	else
 	{
-		printf("Queue is not empty\n");
+		cout<<"Queue is not empty\n";
 	}
-}
-
-void show()
-{
-	printf("Please ,now the turn is:");
-	dequeue();
 }
 
 int main()
 {
+	
 	int choice;
 	while(1)
 	{
 		printf("\n\tEnter the choice:\n");
 		printf("1-Enqueue Operation\n");
+		printf("2-Dequeue Operation\n");
 		printf("3-Display\n");
-		printf("4-Check whether the queue is empty or not\n");
+		printf("4-To check whether the queue is empty or not\n");
 		printf("5-Exit\n");
-		printf("6-Turn of\n");
 		scanf("%d",&choice);
 		switch(choice)
 		{
 			case 1:
 				enqueue();
 				break;
-			
 			case 2:
-				display();
+				dequeue();
 				break;
 			case 3:
-				isEmpty();
+				display();
 				break;
 			case 4:
-				exit(0);
+				isEmpty();
+				break;
 			case 5:
-				show();
+				exit(0);
 			default:
 				printf("Invalid Choice\n");
 		}
 	}
 	return 0;
 }
+
